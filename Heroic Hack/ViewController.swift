231cc2ff68ef96905b7783040166502b5896f8a6
Callen71 <callen71@gatech.edu>
2018-10-20 17:07:22 -0400

    Initial Commit

---
+++ Heroic Hack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Heroic Hack
+//
+//  Created by Courtney Storm on 10/20/18.
+//  Copyright © 2018 Courtney Storm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
